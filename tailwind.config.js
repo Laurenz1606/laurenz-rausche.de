0a2725f6d54f0ad29bea1da729d34fca34733faa
Laurenz1606 <laurenz.rausche@gmail.com>
2021-08-02 11:19:10 +0000

    tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: "class", // or 'media' or 'class'
-  theme: {},
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
